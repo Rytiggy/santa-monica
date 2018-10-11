f313d565d821bc2ae60c4b5d1975d9e2476169d9
Halee Mason <halee.mason@gmail.com>
2018-10-10 22:59:26 -0400

    Set up logging. Not sure what I am going to work on yet.

---
+++ src/main/python/get_data.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+# Configuration variable names as constants
+APP_NAME = 'MY_APP_NAME'
+APP_AWS_KEY = 'MY_APP_AWS_KEY'
+APP_AWS_SECRET_KEY = 'MY_APP_AWS_SECRET_KEY'
+
