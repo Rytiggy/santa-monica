e2f8bbbe1b7446e917790aa2b515f8f381fa7ecb
Halee Mason <halee.mason@gmail.com>
2018-10-27 11:47:58 -0400

    Small changes to clean up the file and remove .ipynb line breaks.

--- src/main/python/riot_api_connection.py
+++ src/main/python/riot_api_connection.py
@@ -1,8 +1,4 @@
-
 # coding: utf-8
-
-# In[34]:
-
 
 import requests
 
@@ -11,35 +7,14 @@ summoner_miss = 'Miss'
 summoner_slayre = 'Slayre'
 summoner_by_name = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key=".format(summoner_slayre)
 
-
-# In[35]:
-
-
 full_endpoint = summoner_by_name + my_api_key
 full_endpoint
 
 
-# In[36]:
-
-
 response = requests.get(full_endpoint)
-
-
-# In[27]:
-
-
 type(response)
 
 
-# In[37]:
-
-
 my_summoner_info = response.json()
-my_summoner_info['id']
-
-
-# In[31]:
-
-
 my_summoner_info['id']
 
